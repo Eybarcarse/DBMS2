b0d324024fa7d870d888d88c2ce74f21867021e2
Eybarcarse <130945832+Eybarcarse@users.noreply.github.com>
2023-11-09 18:05:28 -1200

    Add files via upload

---
+++ myshop/delete.php
@@ -0,0 +1,20 @@
+<?php
+ $servername="localhost";
+ $username="root";
+ $password="";
+ $database="myshop";
+
+ $connection = new mysqli ($servername,$username,$password,$database );
+    
+ $id = $_GET['id'];
+ $connection = new PDO ('mysql:host=localhost;dbname=myshop','root','');
+ if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = 'DELETE FROM clients WHERE id=:id';
+$delete = $connection->prepare($sql);
+$delete -> bindValue ('id',$id);
+$delete -> execute();
+header ('location:/myshop/index.php');
+
+?>
